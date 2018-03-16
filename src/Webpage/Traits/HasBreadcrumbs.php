42864244f68b0348ec2704421d57d1f98ca63880
Marco Mark <marco@silvanite.com>
2018-03-16 11:55:36 +0000

    Append breadcrumbs attribute using maxfactor trait constructor

--- src/Webpage/Traits/HasBreadcrumbs.php
+++ src/Webpage/Traits/HasBreadcrumbs.php
@@ -5,6 +5,23 @@ namespace Maxfactor\Support\Webpage\Traits;
 trait HasBreadcrumbs
 {
     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appendsBreadcrumbFields = [
+        'breadcrumbs',
+    ];
+
+    /**
+     * Called by constructor to load appends fields.
+     */
+    public function initHasBreadcrumbs()
+    {
+        $this->appends = array_merge($this->appends, $this->appendsBreadcrumbFields);
+    }
+
+    /**
      * Provides the breadcrumb for the front-end to render
      *
      * @return void
@@ -17,5 +34,10 @@ trait HasBreadcrumbs
                 'url' => '/'
             ],
         ]);
+    }
+
+    public function getBreadcrumbsAttribute()
+    {
+        return $this->seed();
     }
 }
