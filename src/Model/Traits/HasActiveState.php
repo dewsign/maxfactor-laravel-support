cdb0ca6b8897bd6bdb0e1717c12d7b7c3cad9127
Marco Mark <marco@silvanite.com>
2018-07-20 09:48:16 +0100

    Trait to simplify adding active toggle states to a model

---
+++ src/Model/Traits/HasActiveState.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Maxfactor\Support\Model\Traits;
+
+trait HasActiveState
+{
+    private $hasActiveStateFillableFields = [
+        'active',
+    ];
+
+    private $hasActiveStateCastFields = [
+        'active' => 'boolean',
+    ];
+
+    /**
+     * Called by constructor
+     * Load fillable and casts fields
+     */
+    public function bootHasActiveState()
+    {
+        $this->fillable = array_merge($this->fillable, $this->hasActiveStateFillableFields);
+        $this->casts = array_merge($this->casts, $this->hasActiveStateCastFields);
+    }
+
+    /**
+     * Get only active items
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('active', '=', 1);
+    }
+
+
+    /**
+     * Get all items which are not active (as a result, inactive)
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeInactive($query)
+    {
+        return $query->where('active', '!=', 1);
+    }
+}
