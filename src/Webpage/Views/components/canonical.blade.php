6eef04f22aeaa2c97abfd6abd1ab5b9b38aa1956
Marco Mark <m2de@outlook.com>
2019-03-18 14:27:41 +0000

    Ensure the canonical URL is valid by rendering a full URL rather than a relative path

--- src/Webpage/Views/components/canonical.blade.php
+++ src/Webpage/Views/components/canonical.blade.php
@@ -1,5 +1,5 @@
 {{-- The $canonicalLink variable is set in the Service Provider via View Composer --}}
 
 @if(isset($canonicalLink) && !empty($canonicalLink))
-    <link rel="canonical" href="{{ $canonicalLink }}" />
+    <link rel="canonical" href="{{ url($canonicalLink) }}" />
 @endif
